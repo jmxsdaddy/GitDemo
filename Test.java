b5d64a4650c35ea6b2642b0361ba34789c3fa78f
jmxsdaddy <178799181@qq.com>
2021-05-08 14:17:59 +0800

    secont time to update by git command

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("done");
-		System.out.println("secondUpdate");
+		System.out.println("second time update by git command");
 	}
 }
